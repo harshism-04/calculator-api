e7e99c683bbaff66e32d3fa0a1e5ca665100853b
harshism-04 <h.upadhyay0406@gmail.com>
2026-08-11 12:58:51 +0530

    service layer injected to controller

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -1,4 +1,5 @@
 package com.example.calculator.controller;
+import com.example.calculator.service.CalculatorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,23 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/calc")
 public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService)
+    {
+        this.calculatorService=calculatorService;
+    }
+
     @GetMapping("/add")
     public int add(@RequestParam int a, @RequestParam int b) {
-        return a + b;
+        return calculatorService.add(a,b);
+    }
+
+    @GetMapping("/sub")
+    public int sub(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.sub(a,b);
     }
 
     @GetMapping("/mul")
-    public int mul(@RequestParam int a, @RequestParam int b){
-        return a * b;
+    public int mul(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.mul(a,b);
     }
+
     @GetMapping("/div")
-    public int div(@RequestParam int a, @RequestParam int b){
-        if(b!=0)
-        return a / b;
-        else return -1;
-    }
-    @GetMapping("/sub")
-    public int sub(@RequestParam int a, @RequestParam int b){
-        return a - b;
+    public int div(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.div(a,b);
     }
     }
---
+++ src/main/java/com/example/calculator/service/CalculatorService.java
@@ -0,0 +1,26 @@
+package com.example.calculator.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+    public int add(int a,int b)
+    {
+        return a+b;
+    }
+
+    public int sub(int a,int b)
+    {
+        return a-b;
+    }
+
+    public int mul(int a,int b)
+    {
+        return a*b;
+    }
+
+    public int div(int a,int b)
+    {
+        return a/b;
+    }
+}
